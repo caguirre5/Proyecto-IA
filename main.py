1e788dcb403e933b0b797b3cedb13dc8d786ff66
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2023-05-18 21:52:19 -0600

    Avances

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import string
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-text = open('texto.txt', encoding='utf-8').read()
+text = open('text.txt', encoding='utf-8').read()
 
 #Convertimos todo a minusculas
 text = text.lower()
@@ -12,3 +13,10 @@ text = text.translate(str.maketrans('', '', string.punctuation))
 tokenized_words = word_tokenize(text, "english")
 
 print(tokenized_words)
+
+
+final_words = []
+
+for word in tokenized_words:
+    if word not in stopwords.words('english'):
+        final_words.append(word)
